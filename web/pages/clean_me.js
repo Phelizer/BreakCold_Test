3d16cc394210b3f4103c0999ae251b38d3f745f2
Oleksandr Chornyi <chornyi.oleksandr@lll.kpi.ua>
2023-08-04 15:08:35 +0200

    Remove all the logic and state from the Messy Component

--- web/pages/clean_me.js
+++ web/pages/clean_me.js
@@ -1,35 +1,30 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import {UserBLoC} from "./users.bloc";
+import {UsersService} from "../services/UserService";
+import { userStoreInstance } from './stores/UsersStore';
 
+// TODO: wrap this component into observer from 'mobx-react-lite' in order to make it reactive
 export default function MessyPage() {
   const router = useRouter();
-  const [data, setData] = React.useState(null);
-  const [loading, setLoading] = React.useState(false);
 
-  React.useEffect(() => {
-    async function getData() {
-      setLoading(true);
-      //fake an api request and wait 5 second
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-      const data = await response.json();
-      setData(data);
-      setLoading(false);
-    }
-      getData();
-  }, []);
+  // TODO: add IoC container
+  const usersService = new UsersService(userStoreInstance);
+  const bloc = new UserBLoC(userStoreInstance, usersService);
 
   return (
     <div style={{ fontFamily: 'Arial', color: 'black', backgroundColor: 'white', padding: '20px' }}>
       <h1 style={{ fontSize: '30px', color: 'blue' }}>Page Title</h1>
-      {loading ? (
+      {bloc.loading ? (
         <p>Loading...</p>
       ) : (
-        data && data?.map((item, i) => (
+        bloc.users && bloc.users?.map((user, i) => (
           <div key={i} style={{ margin: '10px 0', padding: '10px', border: '1px solid black' }}>
-            <h2 style={{ fontSize: '25px' }}>{item.title}</h2>
-            <p>{item.description}</p>
+            <h2 style={{ fontSize: '25px' }}>{user.title}</h2>
+            <p>{user.description}</p>
+            {/* TODO: remove inline callbacks */}
             <button
-              onClick={() => router.push(`/detail/${item.id}`)}
+              onClick={() => router.push(`/detail/${user.id}`)}
               style={{ padding: '10px', backgroundColor: 'green', color: 'white', border: 'none', cursor: 'pointer' }}
             >
               View Detail
