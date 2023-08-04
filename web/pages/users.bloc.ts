25fdb4cce4e93b29d26f24ed88c43185cd4478d4
Oleksandr Chornyi <chornyi.oleksandr@lll.kpi.ua>
2023-08-04 15:07:42 +0200

    Finish UsersBLoC

--- web/pages/users.bloc.ts
+++ web/pages/users.bloc.ts
@@ -1,19 +1,41 @@
-import { makeObservable, observable } from "mobx";
-
-interface Datum {
-  title: string;
-  description: string;
-  id: string;
-}
+import { mockedAPI } from "@/api/mockedAPI";
+import { computed, makeObservable, observable, runInAction } from "mobx";
+import { IUsersStore } from "./stores/UsersStore";
+import { IUserService } from "@/services/UserService";
 
 export class UserBLoC {
-  constructor() {
+  constructor(
+    private readonly usersStore: IUsersStore,
+    private readonly userService: IUserService
+  ) {
     makeObservable(this, {
-      data: observable,
       loading: observable,
+      errorMsg: observable,
+      users: computed,
     });
+
+    this.init();
   }
 
-  data: Datum[] = [];
   loading = false;
+  errorMsg = "";
+
+  get users() {
+    return this.usersStore.users;
+  }
+
+  init = async () => {
+    await runInAction(async () => {
+      this.loading = true;
+
+      try {
+      } catch (error) {
+        // TODO: add validation of the error with typeguard
+        this.errorMsg = error?.message;
+        console.error(error);
+      } finally {
+        this.loading = false;
+      }
+    });
+  };
 }
