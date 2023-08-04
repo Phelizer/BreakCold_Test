8b939cc3b2f2beb39e07bd030e255a01fc361d7b
Oleksandr Chornyi <chornyi.oleksandr@lll.kpi.ua>
2023-08-04 15:10:31 +0200

    Create GET /users endpoint (not finished yet)

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,16 +1,21 @@
-import express, { Application, Request, Response } from 'express';
-import entitiesRouter from './clean_me.js';  // import entities router
+import express, { Application, Request, Response } from "express";
+import entitiesRouter from "./clean_me.js"; // import entities router
+import usersRouter from "./users"; // import the users router
+import bodyParser from "body-parser";
 
 const app: Application = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT: number = 3001;
 
-app.use('/', entitiesRouter);  // use entities router at path '/entities'
+app.use("/", entitiesRouter); // use entities router at path '/entities'
+app.use("/users", usersRouter);
 
-app.use('/', (req: Request, res: Response): void => {
-    res.send('Hello world!');
+app.use("/", (req: Request, res: Response): void => {
+  res.send("Hello world!");
 });
 
 app.listen(PORT, (): void => {
-    console.log('SERVER IS UP ON PORT:', PORT);
+  console.log("SERVER IS UP ON PORT:", PORT);
 });
--- api/src/users.ts
+++ api/src/users.ts
@@ -22,46 +22,9 @@ const readUsersFromFile = (
   });
 };
 
-const writeUsersToFile = (
-  users: any[],
-  errorCallback: (error: Error) => void
-) => {
-  fs.writeFile("users.json", JSON.stringify(users, null, 2), (err) => {
-    if (err) {
-      errorCallback(err);
-    }
-  });
-};
-
 usersRouter.get("/", (req: Request, res: Response) => {
   readUsersFromFile(
     (users) => res.send(users),
-    (error) => res.status(500).send("Error reading users.json file")
-  );
-});
-
-usersRouter.post("/", (req: Request, res: Response) => {
-  const newUser = req.body;
-  readUsersFromFile(
-    (users) => {
-      newUser.id = users.length + 1;
-      users.push(newUser);
-      writeUsersToFile(users, (error) =>
-        res.status(500).send("Error writing to users.json file")
-      );
-      res.status(201).send(newUser);
-    },
-    (error) => res.status(500).send("Error reading users.json file")
-  );
-});
-
-usersRouter.get("/:id", (req: Request, res: Response) => {
-  readUsersFromFile(
-    (users) => {
-      const user = users.find((u: any) => u.id === Number(req.params.id));
-      if (user) res.status(200).send(user);
-      else res.status(404).send({ message: "User not found" });
-    },
     (error) => res.status(500).send("Error reading users.json file")
   );
 });
